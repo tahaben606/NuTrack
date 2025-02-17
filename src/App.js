a63a567792543e2b58455efc7696ab04d72bf7b8
unknown <mehdibenabed37azer@gmail.com>
2025-02-17 22:54:56 +0100

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './reducers/reducer';
-import Navbar from './components/navbar'; // Ensure the component name is PascalCase
-import SliderList from './components/SliderList';
-import HeroAbout from './components/hero-about'; // Ensure the component name is PascalCase
+import React from "react";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./reducers/reducer";
+import Navbar from "./components/navbar"; // Ensure the component name is PascalCase
+import SliderList from "./components/SliderList";
+import HeroAbout from "./components/hero-about"; // Ensure the component name is PascalCase
 
 // Create Redux store
 const store = createStore(rootReducer);
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <Navbar /> {/* Render Navbar at the top */}
+        <Navbar /> {/* R ender Navbar at the top */}
         <SliderList /> {/* Render SliderList below Navbar */}
         <HeroAbout /> {/* Render HeroAbout below SliderList */}
       </div>
