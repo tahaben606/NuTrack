c3242d8ff01fe613788615d7bf11c256f472fb6f
Taha Benissaouia <tahabenissaouia@gmail.com>
2025-03-04 21:22:20 +0000

    we back !

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/home';
 import Store from './pages/store';
 import RecipeDetails from './pages/recipedetails';
 import LoginPage from './pages/login';
+import SignUp from './pages/signup';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/store" element={<Store />} />
         <Route path="/recipe/:id" element={<RecipeDetails />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
