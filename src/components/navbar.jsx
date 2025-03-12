e27b2fb8c898da0277ed82ad346fb2b22d0c1300
Taha Benissaouia <tahabenissaouia@gmail.com>
2025-03-12 21:12:33 +0000

    some style

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './navbar.css';
 
 const Navbar = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+    const location = useLocation();
+    const isNotHomePage = location.pathname !== '/';
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 50);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <nav className="navbar">
-            <div className="nav-container">
+        <header className={`navbar ${isScrolled ? 'scrolled' : ''}`}>
+            <nav className="nav-container">
+                <div className="logo">
+                    {/* logo hna */}
+                </div>
                 <ul className="nav-links">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/store">Store</Link></li>
                     <li><Link to="/about">About Us</Link></li>
                     <li><Link to="/contact">Contact Us</Link></li>
-                    
                 </ul>
                 <div className="nav-icons">
-                    <Link to="/login"><i className="fas fa-user"></i>
-                        <button className="contact-btn">LOGIN</button> </Link>
-                    <Link to="/signup"><button className="contact-btn">SIGNUP</button> </Link>
-                        
+                    <Link to="/login">
+                        <i className="fas fa-user"></i>
+                        <button className="contact-btn">LOGIN</button>
+                    </Link>
+                    <Link to="/signup">
+                        <button className="contact-btn">SIGNUP</button>
+                    </Link>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </header>
     );
 };
 
